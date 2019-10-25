9673da543e10c87cf71aedbd1034a26a33d33c87
chenxibo <1759064214@qq.com>
2019-10-25 16:04:22 +0800

    分页示例

---
+++ src/main/java/com/cx/demo/util/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.cx.demo.util;
+
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.cx.demo.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+
+}
